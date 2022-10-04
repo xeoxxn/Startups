62fcdc7003726161be3e24afd467f2062ff6860d
xeoxxn <104981505+xeoxxn@users.noreply.github.com>
2022-10-04 17:16:45 +0900

    Add files via upload

---
+++ 08_example_2/08_example_2.ino
@@ -0,0 +1,68 @@
+// Arduino pin assignment
+#define PIN_LED 9
+#define PIN_TRIG 12   // sonar sensor TRIGGER
+#define PIN_ECHO 13   // sonar sensor ECHO
+
+// configurable parameters
+#define SND_VEL 346.0     // sound velocity at 24 celsius degree (unit: m/sec)
+#define INTERVAL 25   // sampling interval (unit: msec)
+#define PULSE_DURATION 10 // ultra-sound Pulse Duration (unit: usec)
+#define _DIST_MIN 100.0   // minimum distance to be measured (unit: mm)
+#define _DIST_MAX 300.0   // maximum distance to be measured (unit: mm)
+
+#define TIMEOUT ((INTERVAL / 2) * 1000.0) // maximum echo waiting time (unit: usec)
+#define SCALE (0.001 * 0.5 * SND_VEL) // coefficent to convert duration to distance
+
+unsigned long last_sampling_time;   // unit: msec
+
+void setup() {
+  // initialize GPIO pins
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_TRIG, OUTPUT);  // sonar TRIGGER
+  pinMode(PIN_ECHO, INPUT);   // sonar ECHO
+  digitalWrite(PIN_TRIG, LOW);  // turn-off Sonar 
+  
+  // initialize serial port
+  Serial.begin(57600);
+}
+
+void loop() {
+  float distance;
+
+  if (millis() < (last_sampling_time + INTERVAL))
+    return;
+
+  distance = USS_measure(PIN_TRIG, PIN_ECHO); // read distance
+
+  if(distance > _DIST_MAX){
+    distance = _DIST_MAX + 10;
+    digitalWrite(PIN_LED,1);
+  }
+  else if(distance < _DIST_MIN){
+    distance = _DIST_MIN - 10;
+    digitalWrite(PIN_LED,1);
+  }
+  else{
+    analogWrite(PIN_LED,int((200-distance)*(200-distance)*(0.0255)));
+  }
+  
+  
+
+  Serial.print("Min:");        Serial.print(_DIST_MIN);
+  Serial.print(",distance:");  Serial.print(distance);
+  Serial.print(",Max:");       Serial.print(_DIST_MAX);
+  Serial.println("");
+  
+  
+  last_sampling_time += INTERVAL;
+}
+
+float USS_measure(int TRIG, int ECHO)
+{
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(PULSE_DURATION);
+  digitalWrite(TRIG, LOW);
+  
+  return pulseIn(ECHO, HIGH, TIMEOUT) * SCALE; // unit: mm
+
+}
